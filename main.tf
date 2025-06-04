ce916879f23f147277e0f5805a61023cc10b37fe
Peter Jausovec <peter.jausovec@solo.io>
2025-06-04 12:07:28 -0700

    Add EC2 instance resource to Terraform configuration
    - Use variables for AMI ID and instance type
    - Associate instance with existing VPC, public subnet, and security group
    - Ensure public IP is associated
    - Add outputs for instance ID and public IP

--- main.tf
+++ main.tf
@@ -103,15 +103,16 @@ resource "aws_security_group" "web" {
   }
 }
 
-# EC2 instance will be added here by the AI agent
-# Example:
-# resource "aws_instance" "web" {
-#   ami           = "ami-0c55b159cbfafe1f0"
-#   instance_type = "t2.micro"
-#   subnet_id     = aws_subnet.public.id
-#   security_groups = [aws_security_group.web.id]
-#   
-#   tags = {
-#     Name = "web-server"
-#   }
-# }
+# EC2 instance resource
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "web-server"
+  }
+}
